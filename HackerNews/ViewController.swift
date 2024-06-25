e5a89d9edd38f4e91f7355bbaf3480a8530c4c0a
Siran Li <siranli411@gmail.com>
2024-06-25 17:43:41 -0500

    Initial Commit

---
+++ HackerNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HackerNews
+//
+//  Created by Siran Li on 6/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
